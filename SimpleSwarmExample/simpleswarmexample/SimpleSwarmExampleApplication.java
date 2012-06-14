e1e7db965845412cf87902fc4dc43f2f3e3a9150
Mark Wolfskehl <mark@ubuntu.(none)>
2012-06-14 15:31:36 -0400

    Work in progress.  Moving Bug Swarm Java client to Android.

--- SimpleSwarmExample/simpleswarmexample/SimpleSwarmExampleApplication.java
+++ SimpleSwarmExample/simpleswarmexample/SimpleSwarmExampleApplication.java
@@ -5,9 +5,6 @@ import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.osgi.service.log.LogService;
-
-import com.buglabs.application.ServiceTrackerHelper.ManagedRunnable;
 import com.buglabs.bug.swarm.client.ISwarmJsonMessageListener;
 import com.buglabs.bug.swarm.client.ISwarmSession;
 import com.buglabs.bug.swarm.client.SwarmClientFactory;
@@ -22,25 +19,40 @@ import com.sun.xml.internal.ws.api.model.ExceptionType;
  * By default, the application will only be started when all service dependencies are fulfilled.  For 
  * finer grained service binding logic, see ServiceTrackerHelper.openServiceTracker(BundleContext context, String[] services, Filter filter, ServiceTrackerCustomizer customizer)
  */
-public class SimpleSwarmExampleApplication implements ManagedRunnable {
+public class SimpleSwarmExampleApplication implements Runnable {
 	
+	/*
+	 * apikey: '63a0e565521670b1cf32040bf179e2f904146e81',
+                            resource: '3602159efda226c136854b0e381b00fd49533fe8',
+                            swarms: ['6c67d6493656d32698ec9f820feca33c38d9abf6'],
+	 */
 	String hostname =       "api.bugswarm.net";
+    String participation_key =  "63a0e565521670b1cf32040bf179e2f904146e81";
+    String resource_id =        "3602159efda226c136854b0e381b00fd49533fe8";
+    String[] swarms = new String[]{ "6c67d6493656d32698ec9f820feca33c38d9abf6"  };
+    
+	/* 
+	 * String hostname =       "api.bugswarm.net";
     String participation_key =  "63a0e565521670b1cf32040bf179e2f904146e81";
     String resource_id =        "6dc1eca12cf6461515fb863a925ef5bfa8278c48";
     String[] swarms = new String[]{ "6c67d6493656d32698ec9f820feca33c38d9abf6"  };	
-	private LogService ls;
+	 */
+
+	//	private LogService ls;
 	private Thread myThread;
 	private ISwarmSession producersession;
+	
+	private int theRun = 1;
 
 	@Override
-	public void run(Map<Object, Object> services) {			
-		ls = (LogService) services.get(LogService.class.getName());
+	public void run() {			
+//		ls = (LogService) services.get(LogService.class.getName());
 		ilog("starting...");
 		myThread = Thread.currentThread();
 		
 		try {
 	        //create a session
-	        producersession = SwarmClientFactory.createProductionSession(hostname, participation_key, resource_id, false, false, swarms);
+	        producersession = SwarmClientFactory.createProductionSession(hostname, participation_key, resource_id,  swarms);
 
 	        //add a listener for incoming messages
 	        producersession.addListener(new ISwarmJsonMessageListener() {                   
@@ -67,13 +79,14 @@ public class SimpleSwarmExampleApplication implements ManagedRunnable {
 	        });
 	        ilog("Connected, sending messages");
 
-	        //send some messages
-	        HashMap<String, String> map = new HashMap<String, String>();
-	        map.put("message", "Java client hi");
 	        try {
 	        	while(true){
+	    	        //send some messages
+	    	        HashMap<String, String> map = new HashMap<String, String>();
+	    	        map.put("message-"+theRun, "Java client hi number "+theRun);
 		            producersession.send(map);
 		            Thread.sleep(1000);
+		            theRun++;
 		        }
 	        } catch (InterruptedException e){
 	        	producersession.close();
@@ -87,15 +100,15 @@ public class SimpleSwarmExampleApplication implements ManagedRunnable {
 		
 	}
 
-	@Override
+//	@Override
 	public void shutdown() {
 		// TODO Add shutdown code here if necessary.
 		myThread.interrupt();
 	}
 	
 	//Wrappers for the log service (to standardize logged messages)
-	void ilog(String message){  ls.log(LogService.LOG_INFO, "["+this.getClass().getPackage().getName()+"] "+message);	}
-	void dlog(String message){  ls.log(LogService.LOG_DEBUG, "["+this.getClass().getPackage().getName()+"] "+message);	}
-	void elog(String message){  ls.log(LogService.LOG_ERROR, "["+this.getClass().getPackage().getName()+"] "+message);	}
-	void wlog(String message){  ls.log(LogService.LOG_WARNING, "["+this.getClass().getPackage().getName()+"] "+message);	}
+	void ilog(String message){  System.out.println("["+this.getClass().getPackage().getName()+"] "+message);}
+	void dlog(String message){  System.out.println("["+this.getClass().getPackage().getName()+"] "+message);}
+	void elog(String message){  System.out.println( "["+this.getClass().getPackage().getName()+"] "+message);	}
+	void wlog(String message){  System.out.println( "["+this.getClass().getPackage().getName()+"] "+message); }
 }
